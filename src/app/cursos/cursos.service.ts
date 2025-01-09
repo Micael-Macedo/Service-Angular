fd3ae1b4cb11b580401b2104d537c483d8f6660d
Micael-Macedo <micaelm2009@hotmail.com>
2025-01-09 11:17:51 -0300

    feat: add consoleLog

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { EventEmitter, inject, Injectable } from '@angular/core';
+import { LogService } from '../shared/log.service';
 
 
 @Injectable({
@@ -10,16 +11,18 @@ export class CursosService {
 
   emitirCursoCriado = new EventEmitter<string>();
   static criouNovoCurso = new EventEmitter<string>()
+  private logService = inject(LogService)
 
   constructor() {
-    console.log('CursosService');
   }
 
   getCursos() {
+    this.logService.consoleLog("Obtendo lista de cursos")
     return this.cursos;
   }
 
   addCurso(curso: string) {
+    this.logService.consoleLog("Criando o novo curso "+ curso)
     this.cursos.push(curso);
     this.emitirCursoCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso)
